03fca8e718ac82b3c728b47c8b5c7d5097ebf6f6
Andrey <folganoid@yahoo.com>
2020-01-12 20:50:58 +0200

    (fnc) laravel passport

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,6 +4,7 @@
  * building robust, powerful web applications using Vue and Laravel.
  */
 import DashboardComponent from "./components/DashboardComponent";
+import DeveloperComponent from "./components/DeveloperComponent";
 import ProfileComponent from "./components/ProfileComponent";
 import UsersComponent from "./components/UsersComponent";
 
@@ -44,6 +45,7 @@ Vue.use(VueProgressBar, {
 
 let routes = [
     {path: '/dashboard', component: DashboardComponent},
+    {path: '/developer', component: DeveloperComponent},
     {path: '/profile', component: ProfileComponent},
     {path: '/users', component: UsersComponent},
 ];
@@ -73,6 +75,21 @@ window.Fire = new Vue();
 
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
+
+Vue.component(
+    'passport-clients',
+    require('./components/passport/Clients.vue').default
+);
+
+Vue.component(
+    'passport-authorized-clients',
+    require('./components/passport/AuthorizedClients.vue').default
+);
+
+Vue.component(
+    'passport-personal-access-tokens',
+    require('./components/passport/PersonalAccessTokens.vue').default
+);
 
 Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 
